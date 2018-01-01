b8364cb5cfc6ffef188f66f4a260f7cee7c38dcc
Carl Alberto <cralberto11@gmail.com>
2018-01-01 11:45:53 +0800

    fix phpcs errors

--- web/wp-content/themes/bootstrapfast2/archive.php
+++ web/wp-content/themes/bootstrapfast2/archive.php
@@ -20,23 +20,12 @@ get_header(); ?>
 					the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
+			while ( have_posts() ) {
 				the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
+			}
 			the_posts_navigation();
-
 		} else {
 			get_template_part( 'template-parts/content', 'none' );
 		}
