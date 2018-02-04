7c33ce7b2d6c3316bc47a2d21fd6fa2c89efd56d
Carl Alberto <cralberto11@gmail.com>
2018-02-04 14:25:03 +0800

    arranged the 404 page

--- web/wp-content/themes/bootstrapfast/404.php
+++ web/wp-content/themes/bootstrapfast/404.php
@@ -8,50 +8,55 @@
  */
 
 get_header(); ?>
+	<div class="row">
+		<?php
+		if ( ! bootstrapfast_main_sidebar_placement() ) {
+			get_sidebar();
+		}
+		?>
+		<div id="primary" class="content-area col">
+			<main id="main" class="site-main" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+				<section class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bootstrapfast' ); ?></h1>
+					</header><!-- .page-header -->
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bootstrapfast' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bootstrapfast' ); ?></p>
-					<?php
-					get_search_form();
-					the_widget( 'WP_Widget_Recent_Posts' );
-					if ( bootstrapfast_categorized_blog() ) :
-						?>
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bootstrapfast' ); ?></h2>
-							<ul>
-							<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-							?>
-							</ul>
-						</div><!-- .widget -->
+					<div class="page-content">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bootstrapfast' ); ?></p>
 						<?php
-					endif;
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bootstrapfast' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						get_search_form();
+						the_widget( 'WP_Widget_Recent_Posts' );
+						if ( bootstrapfast_categorized_blog() ) :
+							?>
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bootstrapfast' ); ?></h2>
+								<ul>
+								<?php
+									wp_list_categories( array(
+										'orderby'    => 'count',
+										'order'      => 'DESC',
+										'show_count' => 1,
+										'title_li'   => '',
+										'number'     => 10,
+									) );
+								?>
+								</ul>
+							</div><!-- .widget -->
+							<?php
+						endif;
+							/* translators: %1$s: smiley */
+							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bootstrapfast' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							the_widget( 'WP_Widget_Tag_Cloud' );
+						?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+					</div><!-- .page-content -->
+				</section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #row -->
 <?php
 get_footer();
