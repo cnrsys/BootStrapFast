0b82d5b6e27df9aaee77c49459feac27b5a4790b
Carl Alberto <cralberto11@gmail.com>
2018-02-04 14:43:18 +0800

    added widget to the archive page

--- web/wp-content/themes/bootstrapfast/archive.php
+++ web/wp-content/themes/bootstrapfast/archive.php
@@ -8,32 +8,36 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="row">
 		<?php
-		if ( have_posts() ) {
-		?>
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-			<?php
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( 'template-parts/content', get_post_format() );
-			}
-			the_posts_navigation();
-		} else {
-			get_template_part( 'template-parts/content', 'none' );
+		if ( ! bootstrapfast_main_sidebar_placement() ) {
+			get_sidebar();
 		}
 		?>
+		<div id="primary" class="content-area col">
+			<main id="main" class="site-main" role="main">
+			<?php
+			if ( have_posts() ) {
+			?>
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+				<?php
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'template-parts/content', get_post_format() );
+				}
+				the_posts_navigation();
+			} else {
+				get_template_part( 'template-parts/content', 'none' );
+			}
+			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #row -->
 <?php
-get_sidebar();
 get_footer();
