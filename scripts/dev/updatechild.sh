93da771c5a843a8f29271138f0e74f2b3549f35e
Carl Alberto <cralberto11@gmail.com>
2018-02-04 17:43:18 +0800

    added a script to export child theme to their own repo

---
+++ scripts/dev/updatechild.sh
@@ -0,0 +1,10 @@
+# this copies the latest theme assets that can be utilized by the child themes
+#
+
+yes | cp -R web/wp-content/themes/bootstrapfast/src web/wp-content/themes/bootstrapfastchild/
+
+# yes | cp -rf web/wp-content/themes/bootstrapfast/src/**/* web/wp-content/themes/bootstrapfastchild/src/
+
+#yes | cp -rf web/wp-content/themes/bootstrapfast/src/**/**/* web/wp-content/themes/bootstrapfastchild/src
+
+#yes | cp -rf web/wp-content/themes/bootstrapfast/src/**/**/**/* web/wp-content/themes/bootstrapfastchild/src
