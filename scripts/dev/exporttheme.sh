85908e18f20e891bf2fff43b226b0cf541e1e971
Carl Alberto <cralberto11@gmail.com>
2018-02-04 17:43:34 +0800

    updated the export theme

--- scripts/dev/exporttheme.sh
+++ scripts/dev/exporttheme.sh
@@ -1,1 +1,4 @@
+# this exports theme to be ready for theme submission
+# https://wordpress.org/themes/upload/
+
 zip -r bootstrapfast.zip web/wp-content/themes/bootstrapfast/assets web/wp-content/themes/bootstrapfast/inc web/wp-content/themes/bootstrapfast/template-parts web/wp-content/themes/bootstrapfast/*.php web/wp-content/themes/bootstrapfast/LICENSE web/wp-content/themes/bootstrapfast/readme.txt web/wp-content/themes/bootstrapfast/*.css web/wp-content/themes/bootstrapfast/screenshot.png
